4b98afce20b37204713a535a0cf45a3ae2a20308
Cuates <48161526+Cuates@users.noreply.github.com>
2020-09-04 10:57:25 -0700

    Added Tables, Indexes, And Stored Procedure Files
    Added tables, indexes, and stored procedure SQL files

---
+++ mssql/Table/MediaVideoEncode.sql
@@ -0,0 +1,34 @@
+USE [media]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[MediaVideoEncode](
+	[mveID] [bigint] IDENTITY(1,1) NOT NULL,
+	[videoencode] [nvarchar](100) NOT NULL,
+	[movieInclude] [bit] NOT NULL,
+	[tvInclude] [bit] NOT NULL,
+	[created_date] [datetime2](7) NOT NULL,
+	[modified_date] [datetime2](7) NULL,
+ CONSTRAINT [PK_MediaVideoEncode_videoencode] PRIMARY KEY CLUSTERED 
+(
+	[videoencode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, FILLFACTOR = 90, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[MediaVideoEncode] ADD  DEFAULT (0) FOR [movieInclude]
+GO
+
+ALTER TABLE [dbo].[MediaVideoEncode] ADD  DEFAULT (0) FOR [tvInclude]
+GO
+
+ALTER TABLE [dbo].[MediaVideoEncode] ADD  DEFAULT (getdate()) FOR [created_date]
+GO
+
+ALTER TABLE [dbo].[MediaVideoEncode] ADD  DEFAULT (getdate()) FOR [modified_date]
+GO
---
+++ mssql/Table/TVFeedTemp.sql
@@ -0,0 +1,19 @@
+USE [media]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[TVFeedTemp](
+	[titlelong] [nvarchar](255) NULL,
+	[titleshort] [nvarchar](255) NULL,
+	[publish_date] [datetime2](7) NULL,
+	[created_date] [datetime2](7) NULL
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[TVFeedTemp] ADD  DEFAULT (getdate()) FOR [created_date]
+GO
