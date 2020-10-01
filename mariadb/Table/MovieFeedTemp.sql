e3cf950823234b1e6bb83db6d34b5681594adf24
Cuates <48161526+Cuates@users.noreply.github.com>
2020-09-30 18:47:58 -0700

    Modified Table Schema For More Precision
    Modified table schema for more precision

--- mariadb/Table/MovieFeedTemp.sql
+++ mariadb/Table/MovieFeedTemp.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- ============================
 --        File: MovieFeedTemp
 --     Created: 09/07/2020
---     Updated: 09/28/2020
+--     Updated: 09/30/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie feed temp
@@ -18,5 +18,5 @@ create table if not exists `MovieFeedTemp`(
   `titlelong` varchar(255) collate utf8mb4_unicode_520_ci default null,
   `titleshort` varchar(255) collate utf8mb4_unicode_520_ci default null,
   `publish_date` varchar(255) collate utf8mb4_unicode_520_ci default null,
-  `created_date` datetime default current_timestamp()
+  `created_date` datetime(6) default current_timestamp(6)
 ) engine=InnoDB default charset=utf8mb4 collate utf8mb4_unicode_520_ci;
