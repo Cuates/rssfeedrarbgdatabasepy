f155bb324034e4ff7d89d38b7a9797671a3a2b63
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-05 17:04:34 -0700

    Modified Table Schema Data Type CIText
    Modified table schema data type citext

--- postgresql/Table/TVFeedTemp.sql
+++ postgresql/Table/TVFeedTemp.sql
@@ -4,7 +4,7 @@
 -- ============================
 --        File: TVFeedTemp
 --     Created: 09/07/2020
---     Updated: 09/27/2020
+--     Updated: 10/05/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie feed temp
@@ -15,8 +15,8 @@ drop table if exists TVFeedTemp;
 
 -- Table Create
 create table if not exists TVFeedTemp(
-  titlelong varchar(255) default null,
-  titleshort varchar(255) default null,
+  titlelong citext default null,
+  titleshort citext default null,
   publish_date varchar(255) default null,
   created_date timestamp default current_timestamp
 );
