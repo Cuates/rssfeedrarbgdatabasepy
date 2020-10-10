c13621e0c5440a39fa78c2c7a8f67b0f2ced73ab
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-10 13:06:28 -0700

    Modified Column Data Type Length
    Modified column data type length

--- mariadb/Table/MovieFeed.sql
+++ mariadb/Table/MovieFeed.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- =======================
 --        File: MovieFeed
 --     Created: 09/07/2020
---     Updated: 09/30/2020
+--     Updated: 10/10/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie Feed
@@ -18,7 +18,7 @@ create table if not exists `MovieFeed`(
   `mfID` bigint(20) unsigned not null auto_increment,
   `titlelong` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `titleshort` varchar(255) collate utf8mb4_unicode_520_ci not null,
-  `publish_date` datetime not null,
+  `publish_date` datetime(6) not null,
   `actionstatus` int(11) not null default 0,
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
