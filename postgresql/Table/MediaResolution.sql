f7a57eb5b5760105f1d0e208191bb5dd4dce9ce1
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:55:22 -0700

    Update MediaResolution.sql

--- postgresql/Table/MediaResolution.sql
+++ postgresql/Table/MediaResolution.sql
@@ -2,36 +2,36 @@
 \c <databasename>;
 
 -- =============================
---        File: MediaResolution
+--        File: mediaresolution
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media resolution
 -- =============================
 
 -- Sequence Drop
-drop sequence if exists MediaResolution_mrID_seq;
+drop sequence if exists mediaresolution_mrID_seq;
 
 -- Sequence Create
-create sequence if not exists MediaResolution_mrID_seq;
+create sequence if not exists mediaresolution_mrID_seq;
 
 -- Drop Table
-drop table if exists MediaDynamicRange;
+drop table if exists mediadynamicrange;
 
 -- Table Create
-create table if not exists MediaResolution(
-  mrID bigint not null default nextval('MediaResolution_mrID_seq'),
+create table if not exists mediaresolution(
+  mrID bigint not null default nextval('mediaresolution_mrID_seq'),
   resolution citext not null,
   movieInclude bit(1) not null default b'0',
   tvInclude bit(1) not null default b'0',
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_MediaResolution_resolution primary key (resolution)
+  constraint PK_mediaresolution_resolution primary key (resolution)
 );
 
 -- Sequence Alter ownership
-alter sequence MediaResolution_mrID_seq owned by MediaResolution.mrID;
+alter sequence mediaresolution_mrID_seq owned by mediaresolution.mrID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence MediaResolution_mrID_seq to <username>;
+grant usage, select on sequence mediaresolution_mrID_seq to <username>;
