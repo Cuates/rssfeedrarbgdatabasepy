60da18eee6ee79dc261e8bc923608632d3af47e6
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 14:40:53 -0700

    Update MediaAudioEncode.sql

--- mssql/Table/MediaAudioEncode.sql
+++ mssql/Table/MediaAudioEncode.sql
@@ -18,8 +18,8 @@ go
 create table [dbo].[MediaAudioEncode](
   [maeID] [bigint] identity(1,1) not null,
   [audioencode] [nvarchar](100) not null,
-  [movieInclude] [bit] not null,
-  [tvInclude] [bit] not null,
+  [movieInclude] [tinyint] not null,
+  [tvInclude] [tinyint] not null,
   [created_date] [datetime2](6) not null,
   [modified_date] [datetime2](6) null,
  CONSTRAINT [PK_MediaAudioEncode_audioencode] primary key clustered
