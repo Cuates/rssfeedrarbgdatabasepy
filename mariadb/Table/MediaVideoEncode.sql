049a9e5fc5cfdc56cc0ac454181e757b2bbccc7e
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:05:23 -0700

    Update MediaVideoEncode.sql

--- mariadb/Table/MediaVideoEncode.sql
+++ mariadb/Table/MediaVideoEncode.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- ===============================
 --        File: MediaVideoEncode
 --     Created: 09/07/2020
---     Updated: 09/30/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media video encode
@@ -17,8 +17,8 @@ drop table if exists MediaVideoEncode;
 create table if not exists `MediaVideoEncode`(
   `mveID` bigint(20) unsigned not null auto_increment,
   `videoencode` varchar(255) collate utf8mb4_unicode_520_ci not null,
-  `movieInclude` bit(1) not null default b'0',
-  `tvInclude` bit(1) not null default b'0',
+  `movieInclude` tinyint unsigned not null default 0,
+  `tvInclude` tinyint unsigned not null default 0,
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
   primary key (`mveID`),
