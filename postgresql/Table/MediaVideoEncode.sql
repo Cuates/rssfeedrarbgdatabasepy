3b18f270907df20e018d73912a5d7173ee579b9a
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:57:18 -0700

    Update MediaVideoEncode.sql

--- postgresql/Table/MediaVideoEncode.sql
+++ postgresql/Table/MediaVideoEncode.sql
@@ -2,36 +2,36 @@
 \c <databasename>;
 
 -- ===============================
---        File: MediaVideoEncode
+--        File: mediavideoencode
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media video encode
 -- ===============================
 
 -- Sequence Drop
-drop sequence if exists MediaVideoEncode_mveID_seq;
+drop sequence if exists mediavideoencode_mveID_seq;
 
 -- Sequence Create
-create sequence if not exists MediaVideoEncode_mveID_seq;
+create sequence if not exists mediavideoencode_mveID_seq;
 
 -- Table Drop
-drop table if exists MediaVideoEncode;
+drop table if exists mediavideoencode;
 
 -- Table Create
-create table if not exists MediaVideoEncode(
-  mveID bigint not null default nextval('MediaVideoEncode_mveID_seq'),
+create table if not exists mediavideoencode(
+  mveID bigint not null default nextval('mediavideoencode_mveID_seq'),
   videoencode citext not null,
   movieInclude bit(1) not null default b'0',
   tvInclude bit(1) not null default b'0',
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_MediaVideoEncode_videoencode primary key (videoencode)
+  constraint PK_mediavideoencode_videoencode primary key (videoencode)
 );
 
 -- Sequence Alter ownership
-alter sequence MediaVideoEncode_mveID_seq owned by MediaVideoEncode.mveID;
+alter sequence mediavideoencode_mveID_seq owned by mediavideoencode.mveID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence MediaVideoEncode_mveID_seq to <username>;
+grant usage, select on sequence mediavideoencode_mveID_seq to <username>;
