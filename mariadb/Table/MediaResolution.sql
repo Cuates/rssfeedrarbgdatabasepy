7efd44aa5dbc90de6f5abe55c2d22afbe977393a
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:03:43 -0700

    Update MediaResolution.sql

--- mariadb/Table/MediaResolution.sql
+++ mariadb/Table/MediaResolution.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- =============================
 --        File: MediaResolution
 --     Created: 09/07/2020
---     Updated: 09/30/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media resolution
@@ -17,8 +17,8 @@ drop table if exists MediaResolution;
 create table if not exists `MediaResolution`(
   `mrID` bigint(20) unsigned not null auto_increment,
   `resolution` varchar(255) collate utf8mb4_unicode_520_ci not null,
-  `movieInclude` bit(1) not null default b'0',
-  `tvInclude` bit(1) not null default b'0',
+  `movieInclude` tinyint unsigned not null default 0,
+  `tvInclude` tinyint unsigned not null default 0,
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
   primary key (`mrID`),
