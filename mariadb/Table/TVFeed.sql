543ec93c87ccb2ae6b58fb0816bc7052fe914fd9
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:29:00 -0700

    Update TVFeed.sql

--- mariadb/Table/TVFeed.sql
+++ mariadb/Table/TVFeed.sql
@@ -2,19 +2,19 @@
 use <databasename>;
 
 -- =======================
---        File: TVFeed
+--        File: tvfeed
 --     Created: 09/07/2020
---     Updated: 10/10/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: TV Feed
 -- =======================
 
 -- Table Drop
-drop table if exists TVFeed;
+drop table if exists tvfeed;
 
 -- Table Create
-create table if not exists `TVFeed`(
+create table if not exists `tvfeed`(
   `tfID` bigint(20) unsigned not null auto_increment,
   `titlelong` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `titleshort` varchar(255) collate utf8mb4_unicode_520_ci not null,
@@ -23,6 +23,6 @@ create table if not exists `TVFeed`(
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
   primary key (`tfID`),
-  unique key `UQIX_TVFeed_titlelong` (`titlelong`),
-  index `IX_TVFeed_titleshort` (`titleshort`)
+  unique key `UQIX_tvfeed_titlelong` (`titlelong`),
+  index `IX_tvfeed_titleshort` (`titleshort`)
 ) engine=InnoDB default charset=utf8mb4 collate utf8mb4_unicode_520_ci;
