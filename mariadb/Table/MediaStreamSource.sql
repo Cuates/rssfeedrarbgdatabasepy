6b7db887052fecb1c2fb02d8e4681a3534df277f
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:05:02 -0700

    Update MediaStreamSource.sql

--- mariadb/Table/MediaStreamSource.sql
+++ mariadb/Table/MediaStreamSource.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- ================================
 --        File: MediaStreamSource
 --     Created: 09/07/2020
---     Updated: 09/30/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media stream source
@@ -17,8 +17,9 @@ drop table if exists MediaStreamSource;
 create table if not exists `MediaStreamSource`(
   `mssID` bigint(20) unsigned not null auto_increment,
   `streamsource` varchar(255) collate utf8mb4_unicode_520_ci not null,
-  `movieInclude` bit(1) not null default b'0',
-  `tvInclude` bit(1) not null default b'0',
+  `streamdescription` varchar(255) collate utf8mb4_unicode_520_ci not null,
+  `movieInclude` tinyint unsigned not null default 0,
+  `tvInclude` tinyint unsigned not null default 0,
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
   primary key (`mssID`),
