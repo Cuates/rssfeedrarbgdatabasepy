2552eb49e4f38a771b1881bcf32dc75d498af6ab
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:54:14 -0700

    Update MediaDynamicRange.sql

--- postgresql/Table/MediaDynamicRange.sql
+++ postgresql/Table/MediaDynamicRange.sql
@@ -2,36 +2,36 @@
 \c <databasename>;
 
 -- ================================
---        File: MediaDynamicRange
+--        File: mediadynamicrange
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media dynamic range
 -- ================================
 
 -- Sequence Drop
-drop sequence if exists MediaDynamicRange_mdrID_seq;
+drop sequence if exists mediadynamicrange_mdrID_seq;
 
 -- Sequence Create
-create sequence if not exists MediaDynamicRange_mdrID_seq;
+create sequence if not exists mediadynamicrange_mdrID_seq;
 
 -- Drop Table
-drop table if exists MediaAudioEncode;
+drop table if exists mediaaudioencode;
 
 -- Table Create
-create table if not exists MediaDynamicRange(
-  mdrID bigint not null default nextval('MediaDynamicRange_mdrID_seq'),
+create table if not exists mediadynamicrange(
+  mdrID bigint not null default nextval('mediadynamicrange_mdrID_seq'),
   dynamicrange citext not null,
   movieInclude bit(1) not null default b'0',
   tvInclude bit(1) not null default b'0',
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_MediaDynamicRange_dynamicrange primary key (dynamicrange)
+  constraint PK_mediadynamicrange_dynamicrange primary key (dynamicrange)
 );
 
 -- Sequence Alter ownership
-alter sequence MediaDynamicRange_mdrID_seq owned by MediaDynamicRange.mdrID;
+alter sequence mediadynamicrange_mdrID_seq owned by mediadynamicrange.mdrID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence MediaDynamicRange_mdrID_seq to <username>;
+grant usage, select on sequence mediadynamicrange_mdrID_seq to <username>;
