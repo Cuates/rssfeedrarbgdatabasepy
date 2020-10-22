8b2e0b4a6b29aa08b446cb705a52a9b276577bd8
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:52:50 -0700

    Update MediaAudioEncode.sql

--- postgresql/Table/MediaAudioEncode.sql
+++ postgresql/Table/MediaAudioEncode.sql
@@ -2,36 +2,36 @@
 \c <databasename>;
 
 -- ===============================
---        File: MediaAudioEncode
+--        File: mediaaudioencode
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media audio encode
 -- ===============================
 
 -- Sequence Drop
-drop sequence if exists MediaAudioEncode_maeID_seq;
+drop sequence if exists mediaaudioencode_maeID_seq;
 
 -- Sequence Create
-create sequence if not exists MediaAudioEncode_maeID_seq;
+create sequence if not exists mediaaudioencode_maeID_seq;
 
 -- Table Drop
-drop table if exists MediaAudioEncode;
+drop table if exists mediaaudioencode;
 
 -- Table Create
-create table if not exists MediaAudioEncode(
-  maeID bigint not null default nextval('MediaAudioEncode_maeID_seq'),
+create table if not exists mediaaudioencode(
+  maeID bigint not null default nextval('mediaaudioencode_maeID_seq'),
   audioencode citext not null,
   movieInclude bit(1) not null default b'0',
   tvInclude bit(1) not null default b'0',
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_MediaAudioEncode_audioencode primary key (audioencode)
+  constraint PK_mediaaudioencode_audioencode primary key (audioencode)
 );
 
 -- Sequence Alter ownership
-alter sequence MediaAudioEncode_maeID_seq owned by MediaAudioEncode.maeID;
+alter sequence mediaaudioencode_maeID_seq owned by mediaaudioencode.maeID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence MediaAudioEncode_maeID_seq to <username>;
+grant usage, select on sequence mediaaudioencode_maeID_seq to <username>;
