414f603a2969180d66a957b327852247f1cd011a
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:58:39 -0700

    Update TVFeed.sql

--- postgresql/Table/TVFeed.sql
+++ postgresql/Table/TVFeed.sql
@@ -2,37 +2,37 @@
 \c <databasename>;
 
 -- =======================
---        File: TVFeed
+--        File: tvfeed
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: TV Feed
 -- =======================
 
 -- Sequence Drop
-drop sequence if exists TVFeed_tfID_seq;
+drop sequence if exists tvfeed_tfID_seq;
 
 -- Sequence Create
-create sequence if not exists TVFeed_tfID_seq;
+create sequence if not exists tvfeed_tfID_seq;
 
 -- Table Drop
-drop table if exists TVFeed;
+drop table if exists tvfeed;
 
 -- Table Create
-create table if not exists TVFeed(
-  tfID bigint not null default nextval('TVFeed_tfID_seq'),
+create table if not exists tvfeed(
+  tfID bigint not null default nextval('tvfeed_tfID_seq'),
   titlelong citext not null,
   titleshort citext not null,
   publish_date timestamp not null,
   actionstatus int not null,
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_TVFeed_titlelong primary key (titlelong)
+  constraint PK_tvfeed_titlelong primary key (titlelong)
 );
 
 -- Sequence Alter ownership
-alter sequence TVFeed_tfID_seq owned by TVFeed.tfID;
+alter sequence tvfeed_tfID_seq owned by tvfeed.tfID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence TVFeed_tfID_seq to <username>;
+grant usage, select on sequence tvfeed_tfID_seq to <username>;
