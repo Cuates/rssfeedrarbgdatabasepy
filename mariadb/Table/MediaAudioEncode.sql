b0fdf23b0db993d8d788839f2ec4d921fef1f391
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:03:05 -0700

    Update MediaAudioEncode.sql

--- mariadb/Table/MediaAudioEncode.sql
+++ mariadb/Table/MediaAudioEncode.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- ===============================
 --        File: MediaAudioEncode
 --     Created: 09/07/2020
---     Updated: 09/30/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media audio encode
@@ -17,8 +17,8 @@ drop table if exists MediaAudioEncode;
 create table if not exists `MediaAudioEncode`(
   `maeID` bigint(20) unsigned not null auto_increment,
   `audioencode` varchar(255) collate utf8mb4_unicode_520_ci not null,
-  `movieInclude` bit(1) not null default b'0',
-  `tvInclude` bit(1) not null default b'0',
+  `movieInclude` tinyint unsigned not null default 0,
+  `tvInclude` tinyint unsigned not null default 0,
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
   primary key (`maeID`),
