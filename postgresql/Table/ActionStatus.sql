642dddebb0ed4c6258f5eb2529646091e228b3a7
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:51:44 -0700

    Update ActionStatus.sql

--- postgresql/Table/ActionStatus.sql
+++ postgresql/Table/ActionStatus.sql
@@ -2,35 +2,35 @@
 \c <databasename>;
 
 -- ==========================
---        File: ActionStatus
+--        File: actionstatus
 --     Created: 09/07/2020
---     Updated: 09/27/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Action status
 -- ==========================
 
 -- Sequence Drop
-drop sequence if exists ActionStatus_asID_seq;
+drop sequence if exists actionstatus_asID_seq;
 
 -- Sequence Create
-create sequence if not exists ActionStatus_asID_seq;
+create sequence if not exists actionstatus_asID_seq;
 
 -- Table Drop
-drop table if exists ActionStatus;
+drop table if exists actionstatus;
 
 -- Table Create
-create table if not exists ActionStatus(
-  asID bigint not null default nextval('ActionStatus_asID_seq'),
+create table if not exists actionstatus(
+  asID bigint not null default nextval('actionstatus_asID_seq'),
   actionnumber int not null,
   actiondescription varchar(255) not null,
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_ActionStatus_actionnumber primary key (actionnumber)
+  constraint PK_actionstatus_actionnumber primary key (actionnumber)
 );
 
 -- Sequence Alter ownership
-alter sequence ActionStatus_asID_seq owned by ActionStatus.asID;
+alter sequence actionstatus_asID_seq owned by actionstatus.asID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence ActionStatus_asID_seq to <username>;
+grant usage, select on sequence actionstatus_asID_seq to <username>;
