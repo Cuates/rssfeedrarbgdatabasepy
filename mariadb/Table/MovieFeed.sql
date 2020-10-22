b3f36ffcf77c60eaab833ccdc04bc9b109411646
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-22 16:25:59 -0700

    Update MovieFeed.sql

--- mariadb/Table/MovieFeed.sql
+++ mariadb/Table/MovieFeed.sql
@@ -4,17 +4,17 @@ use <databasename>;
 -- =======================
 --        File: MovieFeed
 --     Created: 09/07/2020
---     Updated: 10/10/2020
+--     Updated: 10/22/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie Feed
 -- =======================
 
 -- Table Drop
-drop table if exists MovieFeed;
+drop table if exists moviefeed;
 
 -- Table Create
-create table if not exists `MovieFeed`(
+create table if not exists `moviefeed`(
   `mfID` bigint(20) unsigned not null auto_increment,
   `titlelong` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `titleshort` varchar(255) collate utf8mb4_unicode_520_ci not null,
@@ -23,6 +23,6 @@ create table if not exists `MovieFeed`(
   `created_date` datetime(6) not null default current_timestamp(6),
   `modified_date` datetime(6) default current_timestamp(6),
   primary key (`mfID`),
-  unique key `UQIX_MovieFeed_titlelong` (`titlelong`),
-  index `IX_MovieFeed_titleshort` (`titleshort`)
+  unique key `UQIX_moviefeed_titlelong` (`titlelong`),
+  index `IX_moviefeed_titleshort` (`titleshort`)
 ) engine=InnoDB default charset=utf8mb4 collate utf8mb4_unicode_520_ci;
