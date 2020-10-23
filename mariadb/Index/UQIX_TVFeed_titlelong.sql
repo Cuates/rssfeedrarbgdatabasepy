abc464946195e370b262f416c5a628a0c6ea0351
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 09:17:18 -0700

    Update UQIX_TVFeed_titlelong.sql

--- mariadb/Index/UQIX_TVFeed_titlelong.sql
+++ mariadb/Index/UQIX_TVFeed_titlelong.sql
@@ -2,16 +2,16 @@
 use <databasename>;
 
 -- ========================================
---        File: UQIX_TVFeed_titlelong
+--        File: uqix_tvfeed_titlelong
 --     Created: 09/07/2020
---     Updated: 09/28/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Index tv feed title long
 -- ========================================
 
 -- Index Drop
-drop index if exists `UQIX_TVFeed_titlelong` on TVFeed;
+drop index if exists `uqix_tvfeed_titlelong` on tvfeed;
 
 -- Index Create
-create unique index `UQIX_TVFeed_titlelong` on TVFeed (`titlelong`);
+create unique index `uqix_tvfeed_titlelong` on tvfeed (`titlelong`);
