e261445179cd50a99303054417607e8e59c171b4
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 09:14:19 -0700

    Update UQIX_MediaDynamicRange_dynamicrange.sql

--- mariadb/Index/UQIX_MediaDynamicRange_dynamicrange.sql
+++ mariadb/Index/UQIX_MediaDynamicRange_dynamicrange.sql
@@ -2,16 +2,16 @@
 use <databasename>;
 
 -- ===========================================================
---        File: UQIX_MediaDynamicRange_dynamicrange
+--        File: uqix_mediadynamicrange_dynamicrange
 --     Created: 09/07/2020
---     Updated: 09/28/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Unique index media dynamic range dynamic range
 -- ===========================================================
 
 -- Index Drop
-drop index if exists `UQIX_MediaDynamicRange_dynamicrange` on MediaDynamicRange;
+drop index if exists `uqix_mediadynamicrange_dynamicrange` on mediadynamicrange;
 
 -- Index Create
-create unique index `UQIX_MediaDynamicRange_dynamicrange` on MediaDynamicRange (`dynamicrange`);
+create unique index `uqix_mediadynamicrange_dynamicrange` on mediadynamicrange (`dynamicrange`);
