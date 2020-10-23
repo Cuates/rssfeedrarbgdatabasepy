89dfb9f16fe386ad1a45628a03bd530f1e2da551
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 09:12:54 -0700

    Update IX_TVFeed_titleshort.sql

--- mariadb/Index/IX_TVFeed_titleshort.sql
+++ mariadb/Index/IX_TVFeed_titleshort.sql
@@ -2,16 +2,16 @@
 use <databasename>;
 
 -- =========================================
---        File: IX_TVFeed_titleshort
+--        File: ix_tvfeed_titleshort
 --     Created: 09/07/2020
---     Updated: 09/28/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Index tv feed title short
 -- =========================================
 
 -- Index Drop
-drop index if exists `IX_TVFeed_titleshort` on TVFeed;
+drop index if exists `ix_tvfeed_titleshort` on tvfeed;
 
 -- Index Create
-create index `IX_TVFeed_titleshort` on TVFeed (`titleshort`);
+create index `ix_tvfeed_titleshort` on tvfeed (`titleshort`);
