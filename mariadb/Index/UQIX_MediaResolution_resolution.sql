96248743196b8b3853d77c36cdcd14163cc8049f
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 09:15:21 -0700

    Update UQIX_MediaResolution_resolution.sql

--- mariadb/Index/UQIX_MediaResolution_resolution.sql
+++ mariadb/Index/UQIX_MediaResolution_resolution.sql
@@ -2,16 +2,16 @@
 use <databasename>;
 
 -- =====================================================
---        File: UQIX_MediaResolution_resolution
+--        File: uqix_mediaresolution_resolution
 --     Created: 09/07/2020
---     Updated: 09/28/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Unique index media resolution resolution
 -- =====================================================
 
 -- Index Drop
-drop index if exists `UQIX_MediaResolution_resolution` on MediaResolution;
+drop index if exists `uqix_mediaresolution_resolution` on mediaresolution;
 
 -- Index Create
-create unique index `UQIX_MediaResolution_resolution` on MediaResolution (`resolution`);
+create unique index `uqix_mediaresolution_resolution` on mediaresolution (`resolution`);
