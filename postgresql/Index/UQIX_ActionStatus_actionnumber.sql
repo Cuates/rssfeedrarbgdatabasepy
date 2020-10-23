29664440f1015676ecb0d234f72644bd1aefb9ef
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 08:14:22 -0700

    Update UQIX_ActionStatus_actionnumber.sql

--- postgresql/Index/UQIX_ActionStatus_actionnumber.sql
+++ postgresql/Index/UQIX_ActionStatus_actionnumber.sql
@@ -4,14 +4,14 @@
 -- =====================================================
 --        File: UQIX_ActionStatus_actionnumber
 --     Created: 09/07/2020
---     Updated: 09/27/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Unique index action status action number
 -- =====================================================
 
 -- Index Drop
-drop index if exists UQIX_ActionStatus_actionnumber;
+drop index if exists uqix_actionstatus_actionnumber;
 
 -- Index Create
-create unique index if not exists UQIX_ActionStatus_actionnumber on ActionStatus ((actionnumber));
+create unique index if not exists uqix_actionstatus_actionnumber on actionstatus ((actionnumber));
