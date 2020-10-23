61defb69b1009212f6622a3db2fd2b3fbd348512
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 08:22:40 -0700

    Update UQIX_MediaVideoEncode_videoencode.sql

--- postgresql/Index/UQIX_MediaVideoEncode_videoencode.sql
+++ postgresql/Index/UQIX_MediaVideoEncode_videoencode.sql
@@ -2,16 +2,16 @@
 \c <databasename>;
 
 -- ========================================================
---        File: UQIX_MediaVideoEncode_videoencode
+--        File: uqix_mediavideoencode_videoencode
 --     Created: 09/07/2020
---     Updated: 09/27/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Unique index media video encode videoencode
 -- ========================================================
 
 -- Index Drop
-drop index if exists UQIX_MediaVideoEncode_videoencode;
+drop index if exists uqix_mediavideoencode_videoencode;
 
 -- Index Create
-create unique index if not exists UQIX_MediaVideoEncode_videoencode on MediaVideoEncode ((videoencode));
+create unique index if not exists uqix_mediavideoencode_videoencode on mediavideoencode (videoencode);
