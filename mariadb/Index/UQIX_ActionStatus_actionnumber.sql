20ba2bf3207c272483761262fa95b18c9cf4f554
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 09:11:37 -0700

    Update UQIX_ActionStatus_actionnumber.sql

--- mariadb/Index/UQIX_ActionStatus_actionnumber.sql
+++ mariadb/Index/UQIX_ActionStatus_actionnumber.sql
@@ -2,16 +2,16 @@
 use <databasename>;
 
 -- =====================================================
---        File: UQIX_ActionStatus_actionnumber
+--        File: uqix_actionstatus_actionnumber
 --     Created: 09/07/2020
---     Updated: 09/28/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Unique index action status action number
 -- =====================================================
 
 -- Index Drop
-drop index if exists `UQIX_ActionStatus_actionnumber` on ActionStatus;
+drop index if exists `uqix_actionstatus_actionnumber` on actionstatus;
 
 -- Index Create
-create unique index `UQIX_ActionStatus_actionnumber` on ActionStatus (`actionnumber`);
+create unique index `uqix_actionstatus_actionnumber` on actionstatus (`actionnumber`);
