cbf20c84f3710b0374f613da5fbaa57e33c1e4bb
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 07:49:09 -0700

    Update MediaStreamSource.sql

--- postgresql/Table/MediaStreamSource.sql
+++ postgresql/Table/MediaStreamSource.sql
@@ -4,7 +4,7 @@
 -- ================================
 --        File: mediastreamsource
 --     Created: 09/07/2020
---     Updated: 10/22/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media stream source
@@ -24,8 +24,8 @@ create table if not exists mediastreamsource(
   mssID bigint not null default nextval('mediastreamsource_mssID_seq'),
   streamsource citext not null,
   streamdescription citext not null,
-  movieInclude bit(1) not null default b'0',
-  tvInclude bit(1) not null default b'0',
+  movieInclude smallint not null default 0,
+  tvInclude smallint not null default 0,
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
   constraint PK_mediastreamsource_streamsource primary key (streamsource)
