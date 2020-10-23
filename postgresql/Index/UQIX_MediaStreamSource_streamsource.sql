7ef203b7be80a7ae84bb46d7269f1e1111b38555
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 08:18:02 -0700

    Update UQIX_MediaStreamSource_streamsource.sql

--- postgresql/Index/UQIX_MediaStreamSource_streamsource.sql
+++ postgresql/Index/UQIX_MediaStreamSource_streamsource.sql
@@ -2,16 +2,16 @@
 \c <databasename>;
 
 -- ==========================================================
---        File: UQIX_MediaStreamSource_streamsource
+--        File: uqix_mediastreamsource_streamsource
 --     Created: 09/07/2020
---     Updated: 09/27/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Unique index media stream source streamsource
 -- ==========================================================
 
 -- Index Drop
-drop index if exists UQIX_MediaStreamSource_streamsource;
+drop index if exists uqix_mediastreamsource_streamsource;
 
 -- Index Create
-create unique index if not exists UQIX_MediaStreamSource_streamsource on MediaStreamSource ((streamsource));
+create unique index if not exists uqix_mediastreamsource_streamsource on mediastreamsource (streamsource);
