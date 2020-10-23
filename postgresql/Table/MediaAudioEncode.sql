2dd6ad7124328bcda7f979402a0abd5c34f1a2f9
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 07:47:44 -0700

    Update MediaAudioEncode.sql

--- postgresql/Table/MediaAudioEncode.sql
+++ postgresql/Table/MediaAudioEncode.sql
@@ -4,7 +4,7 @@
 -- ===============================
 --        File: mediaaudioencode
 --     Created: 09/07/2020
---     Updated: 10/22/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media audio encode
@@ -23,8 +23,8 @@ drop table if exists mediaaudioencode;
 create table if not exists mediaaudioencode(
   maeID bigint not null default nextval('mediaaudioencode_maeID_seq'),
   audioencode citext not null,
-  movieInclude bit(1) not null default b'0',
-  tvInclude bit(1) not null default b'0',
+  movieInclude smallint not null default 0,
+  tvInclude smallint not null default 0,
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
   constraint PK_mediaaudioencode_audioencode primary key (audioencode)
