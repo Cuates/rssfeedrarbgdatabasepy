0ef8c34ab07ef469b9e9ebc7c4236896b564e9d9
Cuates <48161526+Cuates@users.noreply.github.com>
2020-10-23 08:03:10 -0700

    Updated Drop Sequence With A Cascade
    Updated drop sequence with a cascade

--- postgresql/Table/MovieFeed.sql
+++ postgresql/Table/MovieFeed.sql
@@ -4,35 +4,35 @@
 -- =======================
 --        File: MovieFeed
 --     Created: 09/07/2020
---     Updated: 10/05/2020
+--     Updated: 10/23/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie Feed
 -- =======================
 
 -- Sequence Drop
-drop sequence if exists MovieFeed_mfID_seq;
+drop sequence if exists moviefeed_mfID_seq cascade;
 
 -- Sequence Create
-create sequence if not exists MovieFeed_mfID_seq;
+create sequence if not exists moviefeed_mfID_seq;
 
 -- Table Drop
-drop table if exists MovieFeed;
+drop table if exists moviefeed;
 
 -- Table Create
-create table if not exists MovieFeed(
-  mfID bigint not null default nextval('MovieFeed_mfID_seq'),
+create table if not exists moviefeed(
+  mfID bigint not null default nextval('moviefeed_mfID_seq'),
   titlelong citext not null,
   titleshort citext not null,
   publish_date timestamp not null,
   actionstatus int not null,
   created_date timestamp not null default current_timestamp,
   modified_date timestamp default current_timestamp,
-  constraint PK_MovieFeed_titlelong primary key (titlelong)
+  constraint pk_moviefeed_titlelong primary key (titlelong)
 );
 
 -- Sequence Alter ownership
-alter sequence MovieFeed_mfID_seq owned by MovieFeed.mfID;
+alter sequence moviefeed_mfID_seq owned by moviefeed.mfID;
 
 -- Grant permission to a sequence
-grant usage, select on sequence MovieFeed_mfID_seq to <username>;
+grant usage, select on sequence moviefeed_mfID_seq to <username>;
