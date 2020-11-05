7cacf130ae6d94f5fb62bd20845b7f226f963cdf
Cuates <48161526+Cuates@users.noreply.github.com>
2020-11-05 15:29:13 -0800

    Updated Table
    Updated table

--- mariadb/Table/moviefeedtemp.sql
+++ mariadb/Table/moviefeedtemp.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- ============================
 --        File: moviefeedtemp
 --     Created: 09/07/2020
---     Updated: 10/22/2020
+--     Updated: 11/05/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie feed temp
@@ -18,5 +18,5 @@ create table if not exists `moviefeedtemp`(
   `titlelong` text collate utf8mb4_unicode_520_ci default null,
   `titleshort` text collate utf8mb4_unicode_520_ci default null,
   `publish_date` text collate utf8mb4_unicode_520_ci default null,
-  `created_date` datetime(6) default current_timestamp(6)
+  `created_date` datetime default current_timestamp(6)
 ) engine=InnoDB default charset=utf8mb4 collate utf8mb4_unicode_520_ci;
