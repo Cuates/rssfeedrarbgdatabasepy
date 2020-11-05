8cc3ce0af081f244563f9fc2d286a912f529bc73
Cuates <48161526+Cuates@users.noreply.github.com>
2020-11-05 15:25:18 -0800

    Updated Table
    Updated table

--- mariadb/Table/mediaresolution.sql
+++ mariadb/Table/mediaresolution.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- =============================
 --        File: mediaresolution
 --     Created: 09/07/2020
---     Updated: 10/23/2020
+--     Updated: 11/05/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media resolution
@@ -19,8 +19,8 @@ create table if not exists `mediaresolution`(
   `resolution` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `movieInclude` tinyint unsigned not null default 0,
   `tvInclude` tinyint unsigned not null default 0,
-  `created_date` datetime(6) not null default current_timestamp(6),
-  `modified_date` datetime(6) default current_timestamp(6),
+  `created_date` datetime not null default current_timestamp(6),
+  `modified_date` datetime default current_timestamp(6),
   primary key (`mrID`),
   unique key `uqix_mediaresolution_resolution` (`resolution`)
 ) engine=InnoDB default charset=utf8mb4 collate utf8mb4_unicode_520_ci;
