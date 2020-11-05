deba3a4e9d00b08256236d799dc18ae2abb7ef09
Cuates <48161526+Cuates@users.noreply.github.com>
2020-11-05 15:30:01 -0800

    Updated Table
    Updated table

--- mariadb/Table/tvfeed.sql
+++ mariadb/Table/tvfeed.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- =======================
 --        File: tvfeed
 --     Created: 09/07/2020
---     Updated: 10/23/2020
+--     Updated: 11/05/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: TV Feed
@@ -20,8 +20,8 @@ create table if not exists `tvfeed`(
   `titleshort` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `publish_date` datetime(6) not null,
   `actionstatus` int(11) not null default 0,
-  `created_date` datetime(6) not null default current_timestamp(6),
-  `modified_date` datetime(6) default current_timestamp(6),
+  `created_date` datetime not null default current_timestamp(6),
+  `modified_date` datetime default current_timestamp(6),
   primary key (`tfID`),
   unique key `uqix_tvfeed_titlelong` (`titlelong`),
   index `ix_tvfeed_titleshort` (`titleshort`)
