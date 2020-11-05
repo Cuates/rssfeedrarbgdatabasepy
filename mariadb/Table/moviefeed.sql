5b076f1f103fb2e047e635b054f64e98ebea93a8
Cuates <48161526+Cuates@users.noreply.github.com>
2020-11-05 15:28:19 -0800

    Updated Table
    Updated table

--- mariadb/Table/moviefeed.sql
+++ mariadb/Table/moviefeed.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- =======================
 --        File: moviefeed
 --     Created: 09/07/2020
---     Updated: 10/23/2020
+--     Updated: 11/05/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Movie Feed
@@ -20,8 +20,8 @@ create table if not exists `moviefeed`(
   `titleshort` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `publish_date` datetime(6) not null,
   `actionstatus` int(11) not null default 0,
-  `created_date` datetime(6) not null default current_timestamp(6),
-  `modified_date` datetime(6) default current_timestamp(6),
+  `created_date` datetime not null default current_timestamp(6),
+  `modified_date` datetime default current_timestamp(6),
   primary key (`mfID`),
   unique key `uqix_moviefeed_titlelong` (`titlelong`),
   index `ix_moviefeed_titleshort` (`titleshort`)
