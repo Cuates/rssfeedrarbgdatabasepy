383c02b1f116581fc92bf7bf4e4bc12fb1b2d6e2
Cuates <48161526+Cuates@users.noreply.github.com>
2020-11-05 15:26:13 -0800

    Updated Table
    Updated table

--- mariadb/Table/mediastreamsource.sql
+++ mariadb/Table/mediastreamsource.sql
@@ -4,7 +4,7 @@ use <databasename>;
 -- ================================
 --        File: mediastreamsource
 --     Created: 09/07/2020
---     Updated: 10/23/2020
+--     Updated: 11/05/2020
 --  Programmer: Cuates
 --   Update By: Cuates
 --     Purpose: Media stream source
@@ -20,8 +20,8 @@ create table if not exists `mediastreamsource`(
   `streamdescription` varchar(255) collate utf8mb4_unicode_520_ci not null,
   `movieInclude` tinyint unsigned not null default 0,
   `tvInclude` tinyint unsigned not null default 0,
-  `created_date` datetime(6) not null default current_timestamp(6),
-  `modified_date` datetime(6) default current_timestamp(6),
+  `created_date` datetime not null default current_timestamp(6),
+  `modified_date` datetime default current_timestamp(6),
   primary key (`mssID`),
   unique key `uqix_mediastreamsource_streamsource` (`streamsource`)
 ) engine=InnoDB default charset=utf8mb4 collate utf8mb4_unicode_520_ci;
