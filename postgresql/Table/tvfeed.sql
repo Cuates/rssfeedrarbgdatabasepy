a537b348b679b7219a2ff83595c193a5c16c9566
Cuates <48161526+Cuates@users.noreply.github.com>
2023-09-10 14:28:29 -0700

    Update tvfeed.sql

--- postgresql/Table/tvfeed.sql
+++ postgresql/Table/tvfeed.sql
@@ -24,6 +24,7 @@ create table if not exists tvfeed(
   tfID bigint not null default nextval('tvfeed_tfID_seq'),
   titlelong citext not null,
   titleshort citext not null,
+  info_url citext default null,
   publish_date timestamp not null,
   actionstatus int not null,
   created_date timestamp not null default current_timestamp,
