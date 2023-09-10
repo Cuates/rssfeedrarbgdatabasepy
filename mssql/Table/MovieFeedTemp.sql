fdc77ba2b0efcfcb60ec6a116721a870ca2ff039
Cuates <48161526+Cuates@users.noreply.github.com>
2023-09-10 14:09:23 -0700

    Update MovieFeedTemp.sql

--- mssql/Table/MovieFeedTemp.sql
+++ mssql/Table/MovieFeedTemp.sql
@@ -18,7 +18,7 @@ go
 create table [dbo].[MovieFeedTemp](
   [titlelong] [nvarchar](max) null,
   [titleshort] [nvarchar](max) null,
-  [info_url] [nvarchar](max) not null,
+  [info_url] [nvarchar](max) null,
   [publish_date] [nvarchar](max) null,
   [created_date] [datetime2](6) null
 ) on [primary]
