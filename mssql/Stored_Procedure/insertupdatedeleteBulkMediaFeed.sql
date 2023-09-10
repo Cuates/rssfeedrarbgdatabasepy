d6f0fbd53160435828799d636eec1a736a32f2b6
Cuates <48161526+Cuates@users.noreply.github.com>
2023-09-10 15:00:33 -0700

    Update insertupdatedeleteBulkMediaFeed.sql

--- mssql/Stored_Procedure/insertupdatedeleteBulkMediaFeed.sql
+++ mssql/Stored_Procedure/insertupdatedeleteBulkMediaFeed.sql
@@ -55,7 +55,7 @@ begin
   set @omitTitleLong = N'-,.,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
   set @omitTitleShort = N'-,.,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
   set @omitPublishDate = N' ,-,/,0,1,2,3,4,5,6,7,8,9,:,.'
-  set @omitInfoUrl = N'-,.,/,%,?,=,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
+  set @omitInfoUrl = N'-,.,/,%,?,=,&,0,1,2,3,4,5,6,7,8,9,A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,[,],_,a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'
   set @maxLengthOptionMode = 255
   set @maxLengthTitleLong = 255
   set @maxLengthTitleShort = 255
